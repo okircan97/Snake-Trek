33a50858068d8458c0558cc64a15e783073c9775
okircan97 <54515394+okircan97@users.noreply.github.com>
2023-09-29 15:00:53 +0300

    Feat: Add a snake model behind the snake menu
    A snake model representing the current ship is added behind the snake menu.
    
    Fixes #3

---
+++ Assets/Scripts - Copy/Effects/TypeText.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+// This class is to create a typing effect for the texts for the game over panel.
+public class TypeText : MonoBehaviour
+{
+    // ////////////////////////////////////////
+    // //////////////// FIELDS ////////////////
+    // ////////////////////////////////////////
+    #region FIELDS
+
+    // Fields for the typing.
+    public float typingSpeed = 50.0f;
+    public string draoclineStr;
+    public string enemyStr;
+    public string asteroidStr;
+    public string scoreStr;
+    bool playTextRunning;
+
+    // References.
+    [SerializeField] TMP_Text draoclineText;
+    [SerializeField] TMP_Text enemyText;
+    [SerializeField] TMP_Text asteroidText;
+    [SerializeField] TMP_Text scoreText;
+
+    #endregion
+
+    // ////////////////////////////////////////
+    // /////////////// METHODS ////////////////
+    // ////////////////////////////////////////
+    #region  METHODS
+
+    // This method is to call the TypeText.
+    public void CallTypeText()
+    {
+        draoclineStr = PlayerPrefs.GetFloat("draoclineCollected").ToString() + " draocline collected.";
+        enemyStr = PlayerPrefs.GetFloat("enemiesDestroyed").ToString() + " enemy destroyed.";
+        asteroidStr = PlayerPrefs.GetFloat("asteroidsDestroyed").ToString() + " asteroids destroyed.";
+        scoreStr = "Score: " + PlayerPrefs.GetFloat("score").ToString();
+
+        Debug.Log("TEXTHANDLER TEXTHANDLER TEXTHANDLER TEXTHANDLER TEXTHANDLER TEXTHANDLER");
+        Debug.Log(draoclineStr);
+        Debug.Log(enemyStr);
+        Debug.Log(asteroidStr);
+        Debug.Log(scoreStr);
+
+        StartCoroutine(PlayText(draoclineText, draoclineStr));
+        StartCoroutine(PlayText(enemyText, enemyStr));
+        StartCoroutine(PlayText(asteroidText, asteroidStr));
+        StartCoroutine(PlayText(scoreText, scoreStr));
+    }
+
+    IEnumerator PlayText(TMP_Text textComponent, string str)
+    {
+        foreach (char c in str)
+        {
+            playTextRunning = true;
+            textComponent.text += c;
+            yield return new WaitForSeconds(0.05f);
+            playTextRunning = false;
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts - Copy/UI/SnakeShowcase.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SnakeShowcase : MonoBehaviour
+{
+    // ////////////////////////////////////////
+    // //////////////// FIELDS ////////////////
+    // ////////////////////////////////////////
+    #region FIELDS
+
+    MainMenuHandler menuHandler;
+    [SerializeField] TMP_Text priceText;
+    [SerializeField] Transform snakeMenu;
+    int snakeIndex = 0;
+    int snakeKey = 0;
+
+    // Price, shield, and speed
+    float[,] snakeProperties = {{0,30,5},{1000,30,5},{1000,30,5.1f},{1500,40,5.2f},{1500,40,5.3f},
+                             {2000,40,5.4f},{2000,40,5.4f},{2500,50,5.5f},{2500,50,5.5f},{3000,50,5.6f},
+                             {3000,50,5.6f},{3500,60,5.6f},{3500,60,5.7f},{4000,70,5.8f},{4000,70,5.9f},
+                             {4500,80,6f},{4500,90,6.1f},{4500,80,6.2f},{5000,90,6.3f},{5000,90,6.3f}};
+
+    #endregion
+
+
+    // ////////////////////////////////////////
+    // //////////// MONO-BEHAVIORS ////////////
+    // ////////////////////////////////////////
+    #region MONOBEHAVIORS
+
+    void Awake()
+    {
+        HandleSnakeShowcase();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuHandler = FindObjectOfType<MainMenuHandler>();
+    }
+
+    #endregion
+
+
+    // ////////////////////////////////////////
+    // /////////////// METHODS ////////////////
+    // ////////////////////////////////////////
+    #region  METHODS
+
+    // This method is to change the snake model according
+    // to the snake key inside the player prefs. It'll also
+    // change the isTrigger property of its colliders to true.
+    public int HandleSnakeShowcase()
+    {
+        int snakeKey = PlayerPrefs.GetInt("SnakeToShow", 0);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == snakeKey)
+            {
+                // Activate the ship model.
+                transform.GetChild(i).gameObject.SetActive(true);
+
+                // Handle the shop texts (for main menu only).
+                HandleShopTexts(i);
+
+                snakeIndex = i;
+            }
+            else
+                transform.GetChild(i).gameObject.SetActive(false);
+        }
+        return snakeIndex;
+    }
+
+    // This method is to handle the shop menu texts, if
+    // the current scene is scene 0.
+    public void HandleShopTexts(int i)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            GameObject.FindWithTag("PriceText").GetComponent<TMP_Text>().text = "Price: " + snakeProperties[i, 0].ToString();
+            GameObject.FindWithTag("ShieldText").GetComponent<TMP_Text>().text = "Shield: " + snakeProperties[i, 1].ToString();
+            GameObject.FindWithTag("SpeedText").GetComponent<TMP_Text>().text = "Speed: " + snakeProperties[i, 2].ToString();
+        }
+    }
+
+    public void BuySnake()
+    {
+        if (PlayerPrefs.GetFloat("credits") >= snakeProperties[snakeIndex, 0])
+        {
+            PlayerPrefs.SetInt("Snake" + snakeIndex.ToString(), 1);
+            Debug.Log("Snake index: " + "Snake" + snakeIndex.ToString());
+            PlayerPrefs.SetFloat("credits", PlayerPrefs.GetFloat("credits") - snakeProperties[snakeIndex, 0]);
+            menuHandler.UnlockSnakeButton(snakeIndex);
+            menuHandler.MoveCamera(snakeMenu);
+            menuHandler.HandleCreditTexts();
+        }
+    }
+
+    #endregion
+}
